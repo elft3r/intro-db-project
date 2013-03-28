4322692f3b59d3d68d45ec1d21267e8fa8ec5dd6
Jochen Zehnder <mail@jochenzehnder.com>
2013-03-28 15:45:34 +0100

    Heroku needs a main class

---
+++ src/ch/ethz/inf/dbproject/DummyMain.java
@@ -0,0 +1,10 @@
+package ch.ethz.inf.dbproject;
+
+public class DummyMain {
+	/**
+	 * Heroku needs a main, but we not so we have this dummy main
+	 */
+	public static void main(String[] args) {
+	}
+
+}
