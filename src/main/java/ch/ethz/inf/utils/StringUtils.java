9b85dad9e09ced9ed4e0dc7b9305d4dd838a5c6d
Jochen Zehnder <mail@jochenzehnder.com>
2013-04-12 14:27:31 +0200

    Resolved conflict

--- src/main/java/ch/ethz/inf/utils/StringUtils.java
+++ src/main/java/ch/ethz/inf/utils/StringUtils.java
@@ -1,11 +1,47 @@
 package ch.ethz.inf.utils;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class StringUtils {
 	public static boolean isNullOrEmpty(String str) {
 		return (str == null || str.trim().isEmpty());
 	}
-	
+
 	public static boolean isNotNullNorEmpty(String str) {
 		return !StringUtils.isNullOrEmpty(str);
+	}
+
+	public static String toString(Map<? extends Object, ? extends Object> map, String delimiter) {
+		String res = "";
+
+		for (Entry<? extends Object, ? extends Object> entry : map.entrySet()) {
+			res += entry.getKey() + "=" + entry.getValue() + delimiter;
+		}
+
+		// when we have a result, we need to remove the last delimiter
+		if (StringUtils.isNotNullNorEmpty(res)) {
+			res = res.substring(0, res.length() - 1);
+		}
+
+		return res;
+	}
+
+	public static String toJSArray(List<String> list) {
+		String res = "[";
+
+		boolean first = true;
+		for (String cat : list) {
+			if (first) {
+				first = false;
+			} else {
+				res += ",";
+			}
+
+			res += "\"" + cat + "\"";
+		}
+
+		return res + "]";
 	}
 }
